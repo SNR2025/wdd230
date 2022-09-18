8782420bf92a2f905259e4a0b5236afbdfa28647
SNR2025 <res21004@byui.edu>
2022-09-18 04:16:49 -0300

    week1k

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,9 @@ function year() {
     let day = new Date();
     let year = day.getFullYear();
     document.getElementById("currentyear").innerHTML = year;
-    console.log(year)
 }
 
 function lastUpdated() {
     let a = document.lastModified;
     document.getElementById("lastmodified").innerHTML = a;
-    console.log(a)
 }
